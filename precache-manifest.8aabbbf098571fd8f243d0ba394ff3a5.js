d048d79ffd560f213382b0754475bdd3bbc47581
bfranckyoobic <bfranck@yoobic.com>
2020-09-10 09:58:18 +0300

    Updates

---
+++ precache-manifest.8aabbbf098571fd8f243d0ba394ff3a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8452d0145f20eadd3c25d4515c886915",
+    "url": "/react-monster-app/index.html"
+  },
+  {
+    "revision": "9d0469a1b5b037e8f730",
+    "url": "/react-monster-app/static/css/main.ddaa9b35.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/react-monster-app/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monster-app/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d0469a1b5b037e8f730",
+    "url": "/react-monster-app/static/js/main.5353e433.chunk.js"
+  },
+  {
+    "revision": "5ea8ddd87e98a6c0c7bb",
+    "url": "/react-monster-app/static/js/runtime-main.545abca0.js"
+  }
+]);
